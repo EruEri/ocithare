1bf266aa8d768d8a38928571c8147276be34a34e
nayu <ndiayeyves@gmail.com>
2024-04-14 23:12:40 +0200

    Initial commit

--- lib/commandline/cshow.ml
+++ lib/commandline/cshow.ml
@@ -19,7 +19,11 @@ open Cmdliner
 
 let name = "show"
 
-type t = { show_password : bool; display_time : int option }
+type t = {
+  show_password : bool;
+  display_time : int option;
+  cithare_file : string option;
+}
 
 let term_display_time =
   Arg.(
@@ -32,24 +36,64 @@ let term_display_time =
 let term_show_password =
   Arg.(value & flag & info [ "show-password" ] ~doc:"Show plain passwords")
 
+let term_cithare_file =
+  Arg.(
+    value
+    & pos 0 (some non_dir_file) None
+    & info [] ~docv:"<CITHARE-CIPHER>" ~doc:"Use $(docv) instead"
+  )
+
 let term_cmd run =
-  let combine show_password display_time =
-    run { show_password; display_time }
+  let combine show_password display_time cithare_file =
+    run { show_password; display_time; cithare_file }
   in
-  Term.(const combine $ term_show_password $ term_display_time)
+  Term.(
+    const combine $ term_show_password $ term_display_time $ term_cithare_file
+  )
 
 let doc = "Display passwords"
-let man = []
+
+let man =
+  [
+    `S Manpage.s_description;
+    `P doc;
+    `P
+      "$(mname)-show(1) shows password records in your terminal in a table \
+       format and by default hides the password string unless \
+       $(b,--show-password) option is provided";
+    `P
+      (Printf.sprintf
+         "$(mname)-show(1) can take a file as parameter. If provided, \
+          $(mname)-show(1) will read the content of this file instead of the \
+          usual $(b,%s)"
+         Libcithare.Config.password_file
+      );
+    `S Manpage.s_examples;
+    `I
+      ( "Read the content of older state of cithare state in \
+         XDG_STATE_HOME/cithare",
+        "$(iname) \"~/.local/state/cithare/cithare Bl0ti4nf 2024-01-01 \
+         00-00-00\""
+      );
+  ]
 
 let cmd run =
   let info = Cmd.info ~doc ~man name in
   Cmd.v info (term_cmd run)
 
 let run t =
-  let { show_password; display_time } = t in
-  let () = Libcithare.Manager.check_initialized () in
+  let { show_password; display_time; cithare_file } = t in
+  let () =
+    match cithare_file with
+    | Some _ ->
+        ()
+    | None ->
+        Libcithare.Manager.check_initialized ()
+  in
   let master_password = Libcithare.Input.ask_password_encrypted () in
-  let manager = Libcithare.Manager.decrypt master_password in
+  let manager =
+    Libcithare.Manager.decrypt ?where:cithare_file master_password
+  in
   let () = Libcithare.Manager.display ~show_password ?display_time manager in
   ()
 
